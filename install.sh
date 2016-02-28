f44a1e1a28803b76e08064489151c5b56ac8c6db
navopw <navopw@users.noreply.github.com>
2016-02-28 15:46:15 +0100

    Removed clear

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ rm -rf /usr/bin/suf
 cat <<EOT >> /usr/bin/suf
 if [ \$# -eq 1 ]; then
         if [ \$(getent passwd \$1) ]; then
-                su "\$1" -c "cd /home/"\$1" && script /dev/null && clear"
+                su "\$1" -c "cd /home/"\$1"; script /dev/null;"
         else
                 echo '[suf] The user "'"\$1"'" does not exist!';
         fi
